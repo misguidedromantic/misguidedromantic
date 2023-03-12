92337e8e166cd11ed88793a2c7a3513a8e00ca64
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2023-03-13 10:46:02 +1100

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,3 +1,12 @@
 window.onload{
-let svg = d3.select("#canvas").append("svg").attr("height", 500).attr("width",500)
+let svg = d3.select("#canvas")
+.append("svg")
+.attr("height", 500)
+.attr("width",500)
+
+svg.append("circle")
+.attr("cx",50)
+.attr("cy",50)
+.attr("r", 10)
+.attr("fill", "blue")
 }
