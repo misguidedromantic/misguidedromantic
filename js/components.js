fd2d88c08c3363bc3de65f33b2f41a48c041e953
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2023-04-22 15:03:17 +1000

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,16 +1,79 @@
-window.onload = function (){
-  
-console.log("hello")
-  
-let svg = d3.select("#canvas")
-.append("svg")
-.style("height", 500)
-.style("width",500)
+let songs = {}
+let svg ={}
 
-svg.append("circle")
-.attr("cx",50)
-.attr("cy",50)
-.attr("r", 10)
-.attr("fill", "blue")
+window.onload = function (){setup()}
+
+function setup(){
+  setupSongs()
+  setupDisplay()
+}
+
+function setupSongs (){
+  songs.goodAfterBad = setupGoodAfterBad()
+  console.log(songs)
+}
+
+function setupDisplay(){
+  setupSVGCanvas()
+  displaySong(songs.goodAfterBad)
+}
+
+function setupSVGCanvas(){
+  
+  svg = d3.select("#canvas")
+    .append("svg")
+    .attr("height", 500)
+    .attr("width",500)
+
+}
+  
+function displaySong(song){
+  
+  svg.selectAll("circle")
+    .data(song)
+    .join("rect")
+    .attr("width", d => d.bars.length)
+    .attr("height", 15)
+    .attr("fill", "green")
+    .attr("opacity", 1)
+    
+}
+
+function setupGoodAfterBad (){
+  
+  let thisSong = new song ("Good After Bad")
+  
+  thisSong.addBarSet(155, "four-four")
+  
+  return thisSong
+  
+}  
+
+
+
+class song {
+  
+  constructor(title){
+    this.title = title
+    this.bars = []
+  }
+  
+  addBarSet(number, timeSignature){
+    
+    for (let i = 0; i < number; i++) { 
+      let thisBar = new bar (timeSignature)
+      this.push(thisBar)
+    }
+    
+  }
+  
+}
+
+class bar {
+  
+  constructor(timeSignature){
+    this.timeSignature = timeSignature
+  }
+  
 
 }
