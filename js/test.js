8470126dcc1792c30117c125b9aae8c652c87204
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-01 17:31:18 +1000

    testing tweaks

--- js/test.js
+++ js/test.js
@@ -2,20 +2,28 @@ function runTests(){
     
     const testSong = new song ("testSong")
 
-    function testClassType (){return testSong instanceof song}
+    function testClassType (){
+        let result = "failed"
+        if(testSong instanceof song){result = "passed"}
+        return this.name & ": " & result
+    }
     
-    function testTitle (){return testSong.title === "testSong"}
+    function testTitle (){
+        let result = "failed"
+        if (testSong.title === "testSong"){result = "passed"}
+        return this.name & ": " & result
+    }
     
     function testLengthMethod (){
         testSong.setLength(1,23)
-
-
-        return (testSong.lengthInSeconds === 83) 
+        let result = "failed"
+        if (testSong.lengthInSeconds === 83){result = "passed"}
+        return this.name & ": " & result
     }
 
-    console.log("Correct class: " & testClassType())
-    console.log("Correct title: " & testTitle())
-    console.log("Correct seconds: " & testLengthMethod())
+    console.log(testClassType())
+    console.log(testTitle())
+    console.log(testLengthMethod())
 
 }
 
