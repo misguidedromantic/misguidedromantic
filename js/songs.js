f00ab29b4b36c8313943be121dea7fa9d4387900
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-01 18:23:12 +1000

    yy

--- js/songs.js
+++ js/songs.js
@@ -23,10 +23,6 @@ class song {
   }
   
   setLength(min,sec){
-
-    console.log(min * 60)
-    console.log(sec)
-
     this.lengthInSeconds = (min * 60) + sec
   }
 }
--- js/test.js
+++ js/test.js
@@ -1,12 +1,20 @@
 function runTests(){
-    console.log ("test10")
-    
+
+    let results = []
+
+    console.log ("test12")
     const testSong = new song ("testSong")
+    
+    console.log(getTestResult(testClassType))
+    
+
 
     function testClassType (){ 
-        let result = "fail"
-        if(testSong instanceof song){result = "pass"}
-        return testClassType.name + ": " + result
+        //let result = "fail"
+        //if(testSong instanceof song){result = "pass"}
+        //return testClassType.name + ": " + result
+
+        return testSong instanceof song
     }
     
     function testTitle (){
@@ -16,15 +24,26 @@ function runTests(){
     }
     
     function testLengthMethod (){
-        testSong.setLength(1,23)
+        testSong.setLength(1, 23)
         let result = "fail"
         if (testSong.lengthInSeconds === 83){result = "pass"}
-        return testTitle.name + ": " + result
+        return testLengthMethod.name + ": " + result
     }
 
     console.log(testClassType())
     console.log(testTitle())
     console.log(testLengthMethod())
 
+}
+
+function getTestResult(func){
+    
+    let result = "fail"
+
+    if(func.call() === true){
+        result = "pass"
+    }
+
+    return func.name + ": " + result
 }
 
