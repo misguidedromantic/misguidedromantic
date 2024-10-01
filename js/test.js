8ca4d2aee432494df0dd8fb7176015389660b290
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-01 17:22:00 +1000

    change logging of test results

--- js/test.js
+++ js/test.js
@@ -1,23 +1,19 @@
 function runTests(){
     
-    let results = []
-
     const testSong = new song ("testSong")
 
     function testClassType (){return testSong instanceof song}
     
-    function testTitle (){return testSong.title = "testSong"}
+    function testTitle (){return testSong.title === "testSong"}
     
     function testLengthMethod (){
         testSong.setLength(1,23)
         return (testSong.lengthInSeconds === 83) 
     }
 
-    results.push(testClassType())
-    results.push(testTitle())
-    results.push(testLengthMethod())
-
-    console.log(results)
+    console.log("Correct class: " & testClassType())
+    console.log("Correct title: " & testTitle())
+    console.log("Correct seconds: " & testLengthMethod())
 
 }
 
