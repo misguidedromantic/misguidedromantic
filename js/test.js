e54d5cc7788ef243a915574cffa1db6ddbd8de0b
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-01 17:57:53 +1000

    t9

--- js/test.js
+++ js/test.js
@@ -1,33 +1,33 @@
 function runTests(){
-    console.log ("test8")
+    console.log ("test9")
     
     const testSong = new song ("testSong")
 
     function testClassType (){ 
         let result = "failed"
-        if(testSong instanceof song){result = "passed"}
+        if(testSong instanceof song){result = "pass"}
         return testClassType.name + ": " + result
     }
     
     function testTitle (){
         let result = "failed"
-        if (testSong.title === "testSong"){result = "passed"}
-        return this.name & ": " & result
+        if (testSong.title === "testSong"){result = "pass"}
+        return this.name + ": " + result
     }
     
     function testLengthMethod (){
         testSong.setLength(1,23)
         let result = "failed"
-        if (testSong.lengthInSeconds === 83){result = "passed"}
-        return this.name & ": " & result
+        if (testSong.lengthInSeconds === 83){result = "pass"}
+        return this.name + ": " + result
     }
     
 
     //testClassType()
 
     console.log(testClassType())
-    //console.log(testTitle())
-    //console.log(testLengthMethod())
+    console.log(testTitle())
+    console.log(testLengthMethod())
 
 }
 
