76a869b0f93e0e5a519217d4450a16b9dcfe480a
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-09 14:53:35 +1100

    test3

--- js/navigation.js
+++ js/navigation.js
@@ -2,22 +2,23 @@ window.onload = function (){setup()}
 
 function setup(){
 
-    const divNavigation = d3.select("#navigation")
-    const svgNavigation = divNavigation
+    let divNavigation = d3.select("#navigation");
+    
+    let svgNavigation = divNavigation
         .append("svg")
         .attr("height", "80px")
-        .attr("width", "200px")
+        .attr("width", "200px");
 
     svgNavigation.append("g")
         .attr("id", "persona")
         .attr("transform", "translate(20, 20)")
         .append("text")
-        .text("persona e.g. mgr")
+        .text("persona e.g. mgr");
 
     svgNavigation.append("g")
         .attr("id", "domain")
         .attr("transform", "translate(80, 20)")
         .append("text")
-        .text("domain")
+        .text("domain");
 
 }
