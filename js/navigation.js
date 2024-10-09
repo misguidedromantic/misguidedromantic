e374dcb4ed24a35a0487fd8b03e1e5da84700c57
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-09 16:15:13 +1100

    stylin

--- js/navigation.js
+++ js/navigation.js
@@ -2,22 +2,24 @@ window.onload = function (){setup()}
 
 async function setup(){
 
-    console.log("setup started")
+    console.log('setup started')
 
-    let divNavigation = d3.select("#navigation");
+    let divNavigation = d3.select('#navigation');
 
     let svgNavigation = divNavigation
-        .append("svg")
-        .attr("height", "80px")
-        .attr("width", "200px");
+        .append('svg')
+        .attr('height', '80px')
+        .attr('width', '200px');
 
-    let persona = new phrase ("persona", "misguided romantic", svgNavigation)
+    let persona = new phrase ('persona', 'misguided romantic', svgNavigation)
     persona.render()
     persona.setPosition(20, 20)
+    persona.setColour("yellow")
 
-    let domain = new phrase ("domain", "garden", svgNavigation)
+    let domain = new phrase ('domain', 'garden', svgNavigation)
     domain.render()
     domain.setPosition(persona.getWidth() + 20 + 5, 20)
+    domain.setColour("red")
 }
 
 
@@ -31,38 +33,50 @@ class phrase {
     }
 
     getGSelection(){
-        return d3.select("#" + this.id)
+        return d3.select('#' + this.id)
+    }
+
+    getTextSelection(){
+        let g = this.getGSelection()
+        return g.select('text')
     }
 
     render(){
-
         let g = this.getGSelection()
 
         if (g.empty()) {
             
-            let g = this.container.append("g")
-                .attr("id", this.id)
+            let g = this.container.append('g')
+                .attr('id', this.id)
                 
-            g.append("text")
-                .text(this.text);
+            g.append('text')
+                .text(this.text)
+                .style('font-family', 'tahoma')
+                .style('font-size', '14px')
+                .style('font-weight', 'bold');
 
 
         } else {
             
-            console.log("no g selection")
+            console.log('no g selection')
 
         }
 
     }
 
     getWidth(){
-        let g = this.getGSelection()
-        return g.select("text").node().getBBox().width;
+        let textElem = this.getTextSelection()
+        return textElem.node().getBBox().width;
     }
 
     setPosition(x, y){
         let g = this.getGSelection()
-        g.attr("transform", "translate(" + x + "," + y + ")")
+        g.attr('transform', 'translate(' + x + ',' + y + ')')
+    }
+
+    setColour(colour){
+        let textElem = this.getTextSelection()
+        textElem.attr('fill', colour)
     }
 
 
